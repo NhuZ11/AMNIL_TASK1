3906e6e3fdb34ff85b498026bfa13ca05a24d435
Nhuz11 <nhuzbajra59@gmail.com>
2025-01-25 21:31:42 +0545

    setup

---
+++ AddUser_7/Frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+        <p>User Form</p>
+
+        <div>
+          <form action="">
+            <label htmlFor="Username">Username</label>
+            <input type="text" className='Username' />
+            
+          </form>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
