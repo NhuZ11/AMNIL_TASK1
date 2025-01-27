e5e42707626c78a510c8b6ce6cbec782f15fbee3
Nhuz11 <nhuzbajra59@gmail.com>
2025-01-27 14:29:29 +0545

    todo using postgresql

---
+++ TodoList_1/Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Inputtodo from './Components/Inputtodo'
+import Listtodo from './Components/Listtodo'
+
+function App() {
+
+  return (
+    <>
+     <Inputtodo />
+     <Listtodo/>
+    </>
+  )
+}
+
+export default App
